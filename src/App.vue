5a2bc1711fd7c2dc5c489cb4756679d75910d990
JuneBuug <junee613@gmail.com>
2020-02-04 11:39:05 +0900

    [수정] proxy vue에 맞게 수정, working

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,45 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <button @click="getAll()">클릭</button>
+    <li v-for="todo in todos">{{todo.data.title}} <span>{{todo.data.completed}}</span> </li>
     <router-view/>
   </div>
 </template>
+
+
+<script>
+
+export default {
+
+  data() {
+    return {
+      todos: [],
+    }
+  },
+  created() {
+    console.log("만들어졌따")
+    this.getAll()
+  },
+  methods: {
+    create() {
+      this.$http.post('/.netlify/functions/todos-create', {'title': 'My todo', completed: false})
+    .then(res => {
+        console.log(res)
+      })
+    },
+    getAll() {
+      this.$http.get('/.netlify/functions/todos-read-all')
+      .then(res => {
+        console.log(res)
+        this.todos = res.data
+        }
+      )
+    }
+  }
+}
+
+</script>
 
 <style>
 #app {
